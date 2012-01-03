f217c212ca6254720d9590e91747a8f77fd7538b
John Anderson <anderson.johnd@gmail.com>
2012-01-02 23:17:59 -0700

    Getting close. Basic flow coming together, just adding an example exercise to finish out implementation.

--- extensions/exercise/Exercise.php
+++ extensions/exercise/Exercise.php
@@ -9,6 +9,7 @@
 namespace li3_exercises\extensions\exercise;
 
 use lithium\analysis\Inspector;
+use lithium\test\Unit;
 
 /**
  * Parent exercise class. Contains logic needed to run the exercise
@@ -25,11 +26,18 @@ class Exercise extends \lithium\core\Object {
 	protected $_autoConfig = array('command', 'methodPrefix');
 	
 	/**
-	 * An Command instance used for I/O.
+	 * Command instance used for I/O.
 	 *
 	 * @var lithium\console\Command
 	 */
 	protected $_command = null;
+	
+	/**
+	 * Unit instance for assertion.
+	 *
+	 * @var lithium\test\Unit
+	 */
+	protected $_unit = null;
 	
 	/**
 	 * Prefix for methods used in exercise.
@@ -46,6 +54,13 @@ class Exercise extends \lithium\core\Object {
 	protected $_steps = array();
 	
 	/**
+	 * Index for the current step.
+	 *
+	 * @var int
+	 */
+	protected $_currentStep = 0;
+	
+	/**
 	 * Exercise init. Each method in the exercise prefixed with $this->_methodPrefix ('explain', 
 	 * by default) is considered a step in the exercise. Steps are run() in the order they were
 	 * originally defined.
@@ -54,6 +69,25 @@ class Exercise extends \lithium\core\Object {
 	 */
 	public function _init() {
 		parent::_init();
+		$this->_initUnit();
+		$this->_initSteps();
+	}
+	
+	/**
+	 * Test unit init. Used for assertions in exercises.
+	 *
+	 * @return void
+	 */
+	protected function _initUnit() {
+		$this->_unit = new Unit();
+	}
+	
+	/**
+	 * Creates step list for this exercise.
+	 * 
+	 * @return void
+	 */
+	protected function _initSteps() {
 		$methods = Inspector::methods($this)->to('array');
 		foreach($methods as $method) {
 			if(substr($method['name'], 0, strlen($this->_methodPrefix)) === $this->_methodPrefix) {
@@ -68,13 +102,164 @@ class Exercise extends \lithium\core\Object {
 	 * @return void
 	 */
  	public function run() {
- 		foreach($this->_steps as $step) {
- 			$this->$step();
- 		}
+		$this->_moveToLatestStep();
+		foreach($this->_steps as $step) {
+			$success = false;
+			while(!$success) {
+				$this->_initUnit();
+				$this->$step();
+				$results = $this->_unit->results();
+				if($this->stepSuccess($results)) {
+					$this->_logLastCompletedStep($step);
+					$this->_command->clear();
+					$success = true;
+				} else {
+					$this->_command->clear();
+					$this->_printErrors($results);
+				}
+			}
+		}
+		$this->header("Exercise complete.");
+		$response = $this->in("{:cyan}Run again?{:end}", 
+		array(
+			'choices' => array('y', 'n'),
+			'default' => 'n'
+		));
+		if($response == 'y') {
+			$this->_command->clear();
+			$this->_clearLogFile();
+			$this->_initSteps();
+			return $this->run();
+		}
  	}
 	
 	/**
-	 * Convenience wrapping for /lithium/console/Command class for easy 
+	 * Prints out errors found in 
+	 *
+	 * @param string $results 
+	 * @return void
+	 * @author John Anderson
+	 */
+	protected function _printErrors($results) {
+		
+		foreach($results as $result) {
+			if($result['result'] === 'fail') {
+				$this->_command->hr();
+				$this->_command->error('{:error}' . $result['message'] . '{:end}');
+			}
+		}
+		$this->_command->hr();
+		$this->_command->out();
+	}
+	
+	/**
+	 * Returns the name of the log file for this exercise.
+	 *
+	 * @return void
+	 * @author John Anderson
+	 */
+	protected function _getLogFileName() {
+		$tmpDir = LITHIUM_APP_PATH . '/resources/tmp';
+		$filename = $tmpDir . '/' . \lithium\util\Inflector::slug(__CLASS__) . '.log';
+		return $filename;
+	}
+	
+	/**
+	 * Removes the log file for this exercise.
+	 *
+	 * @return void
+	 * @author John Anderson
+	 */
+	protected function _clearLogFile() {
+		unlink($this->_getLogFileName());
+	}
+	
+	/**
+	 * Returns the name of the last completed step, or boolean false if none found.
+	 *
+	 * @return void
+	 * @author John Anderson
+	 */
+	protected function _getLastCompletedStep() {
+		$filename = $this->_getLogFileName();
+		if(file_exists($filename)) {
+			$contents = unserialize(file_get_contents($filename));
+			if(isset($contents['step'])) {
+				return($contents['step']);
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Moves to the last completed step, if one is found.
+	 *
+	 * @author John Anderson
+	 */
+	protected function _moveToLatestStep() {
+		$lastStep = $this->_getLastCompletedStep();
+		if($lastStep !== false) {
+			foreach($this->_steps as $number => $name) {
+				unset($this->_steps[$number]);
+				if($name == $lastStep) {
+					return;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Logs last completed step in temporary directory.
+	 *
+	 * @return void
+	 */
+	protected function _logLastCompletedStep($step) {
+		$data = array(
+			'time' => time(),
+			'step' => $step
+		);
+		file_put_contents($this->_getLogFileName(), serialize($data));
+	}
+	
+	/**
+	 * Verifies step results.
+	 *
+	 * @author John Anderson
+	 */
+	protected function stepSuccess($results) {
+		$success = true;
+		foreach($results as $result) {
+			$success = $success && ($result['result'] == 'pass');
+		}
+		return $success;
+	}
+	
+	/**
+	 * Filler output for when you just want the user to work. Ignores input,
+	 * but halts exercise while the user works and waits for input submission.
+	 *
+	 * @param string $message 
+	 * @return void
+	 * @author John Anderson
+	 */
+	public function pause($message = '') {
+		$fillerMessages = array(
+			"Please press ENTER when you're ready.",
+			"Go ahead and press ENTER when you're done.",
+			"Press ENTER to continue.",
+			"When you're done, press ENTER.",
+			"Please press ENTER when to move on."
+		);
+		if($message === '') {
+			$message = $fillerMessages[array_rand($fillerMessages)];
+		}
+		$this->out();
+		$this->in("{:cyan}" . $message . "{:end}");
+	}
+	
+	/**
+	 * Convenience wrapping for /lithium/console/Command and 
+	 * lihtium\test\Unit class for easy assertions and
 	 * command-line input/output and formatting.
 	 *
 	 * @param string $name 
@@ -82,6 +267,10 @@ class Exercise extends \lithium\core\Object {
 	 * @return mixed
 	 */
 	public function __call($name, $arguments) {
-		return $this->_command->invokeMethod($name, $arguments);
+		if(method_exists($this->_command, $name)) {
+			return $this->_command->invokeMethod($name, $arguments);
+		} else {
+			return $this->_unit->invokeMethod($name, $arguments);
+		}
 	}
 }	
--- extensions/exercises/Example.php
+++ extensions/exercises/Example.php
@@ -15,6 +15,74 @@ use li3_exercises\extensions\exercise\Exercise;
  */
 class Example extends Exercise {
 	public function explainIntro() {
-		$this->header('Welcome!');
+		$this->header('Introduction');
+		
+		$this->out("This is an example exercise that shows you how to *create* another exercise! Not too meta for you?");
+		$this->pause();
+	}
+	
+	public function explainCreateFile() {
+		$this->header("Creating the Exercise File");
+		
+		$this->out("The first thing you need to do is create an exercise file. Let's create a new exercise called `Blog.` Do this by creating a new file in {:green}app/extensions/exercises/Blog.php{:end}.\n");
+		$this->out("You'll need to do it in another terminal session or something.");
+		$this->pause();
+		
+		$this->assertTrue(file_exists(LITHIUM_APP_PATH . '/extensions/exercises/Blog.php'), "I can't seem to see the file yet. Make sure you've created a new file at {:green}app/extensions/exercises/Blog.php{:end}.");
+	}
+	
+	public function explainCreateClass() {
+		$this->header("Creating an Exercise");
+		
+		$this->out("Now, let's create a class inside that file. Make sure it has the right namespace, and name the class {:purple}Blog{:end}. Make sure that it extends the {:purple}\li3_exercises\exercise\Exercise{:end} class.");
+		$this->pause();
+		
+		require_once(LITHIUM_APP_PATH . '/extensions/exercises/Blog.php');
+		$classExists = class_exists('\li3_exercises\extensions\exercises\Blog');
+		$this->assertTrue($classExists, "I can't find that class. Make sure it's got the right namespace (li3_exercises\extensions\exercises)!");
+		
+		if($classExists) {
+			$blog = new \li3_exercises\extensions\exercises\Blog();
+			$this->assertTrue(is_a($blog, '\li3_exercises\extensions\exercise\Exercise'), "Oops! Make sure that the new Blog class extends {:green}li3_exercises\extensions\exercise\Exercise{:end}.");
+		}
+	}
+
+	public function explainExplainMethods() {
+		$this->header("The Explain Methods");
+		
+		$this->out("The next step is to create your first `explain` method. When an exercise is run, each user-defined method that begins with 'explain' is run in the order it was defined.\n");
+		$this->out("Let's start by defining a new method called `explainIntro`.");
+		$this->pause();
+		
+		$methods = \lithium\analysis\Inspector::methods('\li3_exercises\extensions\exercises\Blog', 'extents');
+		$this->assertTrue(isset($methods['explainIntro']), "Hmmm. I can't seem to find a method defined on your new Blog class named '{:purple}explainIntro{:end}'. {:error}Make sure it has public visibility!{:end}");
+	}
+	
+	public function explainCommandUsage() {
+		$this->header("Exercises are Commands");
+		
+		$this->out("Now that you've got a method, your first step is letting your student know what's going on. Each instance of `Exercise` can also act much like a command. You can send output to your student by using the `header`, `out`, and other similar methods of the \lithium\console\Command class.\n");
+		$this->out("It's common to start each step with a header. Add a header call to your `explainIntro` method, followed by another call to `out`. This will serve as your introduction.");
+		$this->pause();
+		$intro = $this->_getIntroOutput();
+		
+		$this->assertTrue(strlen($intro) > 1, 'Your `explainIntro` method didn\'t seem to have any output. Try calling {:purple}$this->out(){:end} or {:purple}$this->header(){:end}.');
+		$this->assertTrue(strstr($intro, '------------------------------------') !== false, 'Did you forget to call {:purple}$this->header(){:end}{:error}?{:end}');
+	}
+	
+	public function explainUnitUsage() {
+		
+	}
+	
+	protected function _getIntroOutput() {
+		$tmpFile = LITHIUM_APP_PATH . '/resources/tmp/' . uniqid() . '.tmp';
+		$output = fopen($tmpFile, 'x');
+		$response = new \lithium\console\Response(compact('output'));
+		$command = new \lithium\console\Command(compact('response'));
+		$blog = new \li3_exercises\extensions\exercises\Blog(compact('command'));
+		$blog->explainIntro();
+		$intro = file_get_contents($tmpFile);
+		unlink($tmpFile);
+		return $intro;
 	}
 }
